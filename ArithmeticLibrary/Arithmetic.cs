425a99160a3fbf3d50b2bd0c2049c0f235ff81cf
Nick <nick_r_moore@hotmail.com>
2023-09-16 19:08:37 -0400

    Added some basic arithmetic methods and unit tests. Renamed Class1

---
+++ ArithmeticLibrary/Arithmetic.cs
@@ -0,0 +1,48 @@
+﻿namespace ArithmeticLibrary
+{
+    public class Arithmetic
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Substract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        public int Modulus(int a, int b)
+        {
+            return a % b;
+        }
+
+        public int Increment(int a)
+        {
+            return ++a;
+        }
+
+        public int Decrement(int a)
+        {
+            return --a;
+        }
+
+        public int Negate(int a)
+        {
+            return -a;
+        }
+
+
+    }
+    
+}
---
+++ ArithmeticLibraryTests/ArithmeticTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArithmeticLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ArithmeticLibrary.Tests
+{
+
+    [TestClass()]
+    public class ArithmeticTests
+    {
+        readonly Arithmetic _arith = new();
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.IsTrue(_arith.Add(1, 2) == 3);
+        }
+
+        [TestMethod()]
+        public void SubstractTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DivideTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ModulusTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IncrementTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DecrementTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void NegateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
