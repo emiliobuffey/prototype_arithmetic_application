7b9b7a6ba3c1428433ce3363c1444438a3934e37
Nick <nick_r_moore@hotmail.com>
2023-09-16 19:56:49 -0400

    Unit Test Update

--- ArithmeticLibraryTests/ArithmeticTests.cs
+++ ArithmeticLibraryTests/ArithmeticTests.cs
@@ -18,49 +18,70 @@ namespace ArithmeticLibrary.Tests
         [TestMethod()]
         public void AddTest()
         {
-            Assert.IsTrue(_arith.Add(1, 2) == 3);
+            var num1 = 1;
+            var num2 = 2;
+
+            Assert.IsTrue(_arith.Add(num1, num2) == num1 + num2);
         }
 
         [TestMethod()]
         public void SubstractTest()
         {
-            Assert.Fail();
+            var num1 = 3;
+            var num2 = 1;
+            
+            Assert.IsTrue(_arith.Substract(num1, num2) == num1 - num2);
         }
 
         [TestMethod()]
         public void MultiplyTest()
         {
-            Assert.Fail();
+            var num1 = 3;
+            var num2 = 2;
+
+            Assert.IsTrue(_arith.Multiply(num1, num2) == num1 * num2);
         }
 
         [TestMethod()]
         public void DivideTest()
         {
-            Assert.Fail();
+            var num1 = 10;
+            var num2 = 5;
+
+            Assert.IsTrue(_arith.Divide(num1, num2) == num1 / num2);
         }
 
         [TestMethod()]
         public void ModulusTest()
         {
-            Assert.Fail();
+            var num1 = 6;
+            var num2 = 3;
+
+            Assert.IsTrue(_arith.Modulus(num1, num2) == num1 % num2);
         }
 
         [TestMethod()]
         public void IncrementTest()
         {
-            Assert.Fail();
+            var num1 = 1;
+
+            Assert.IsTrue(_arith.Increment(num1) == ++num1);
         }
 
         [TestMethod()]
         public void DecrementTest()
         {
-            Assert.Fail();
+            var num1 = 1;
+
+            Assert.IsTrue(_arith.Decrement(num1) == --num1);
         }
 
         [TestMethod()]
         public void NegateTest()
         {
-            Assert.Fail();
+            var num1 = 1;
+
+            Assert.IsTrue(_arith.Negate(num1) == -num1);
         }
     }
 }
